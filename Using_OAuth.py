a14bb158b96ab6a48c251de31f96a967e28e8d9a
Elisa Warner <elisawa@umich.edu>
2017-10-25 11:36:55 -0400

    Added Using_OAuth.py

---
+++ Using_OAuth.py
@@ -0,0 +1,2 @@
+# Using_OAuth.py
+# Add a file that imports and uses functions from the sample_oauth_code.py to try to make an interactive program to access the Tumblr API: https://www.tumblr.com/docs/en/api/v2
